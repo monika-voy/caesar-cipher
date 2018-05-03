a542bbee3597f5182eee263581449a7cf324a553
moon <moon>
2018-05-04 00:28:01 +0200

    Implement decryption method

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,3 +6,12 @@ def cipher(text,rot):
         character = ord(character)
         ciphertxt += chr(character + rot)
     print(ciphertxt)
+
+def decipher(text, rot):
+
+    deciphertxt = ''
+
+    for character in text:
+        character = ord(character)
+        deciphertxt += chr(character - rot)
+    print(deciphertxt)
